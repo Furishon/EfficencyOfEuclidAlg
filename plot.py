5f72c7d543314ea2c1be1781bc1d0e5e55b2a8b9
Furishon <goransson.erik2001@gmail.com>
2019-08-10 19:02:14 +0200

    Implemented euclidAlg and vizualisation

---
+++ plot.py
@@ -0,0 +1,21 @@
+from euclidAlg import findGCD
+from matplotlib import pyplot as plt
+import numpy as np
+import seaborn as sns
+
+bnd = 5000
+
+val = np.empty((bnd, bnd), dtype='uint8')
+for index, gcd in np.ndenumerate(val):
+    val[index[0]][index[1]] = findGCD(index[0], index[1])
+
+ax = sns.heatmap(val,
+     square=True,
+     cmap="YlGnBu",
+     xticklabels=int(bnd/5),
+     yticklabels=int(bnd/5))
+
+ax.set(xlabel='a', ylabel='b')
+plt.title('Iterations required to find gcd(a, b) by the Euclidean Algorithm')
+plt.show()
+
